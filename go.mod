a7f8ee0a4480befdaa8eaf0f8e8dccb494d8f5a2
artaasadi <arta-c@kaytu.io>
2023-05-17 12:45:21 +0330

    get repo file content

---
+++ go.mod
@@ -0,0 +1,21 @@
+module test-github-app
+
+go 1.20
+
+require (
+	github.com/bradleyfalzon/ghinstallation v1.1.1
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/google/go-github/v38 v38.1.0
+	golang.org/x/oauth2 v0.8.0
+)
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-github/v29 v29.0.2 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"crypto/rsa"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/bradleyfalzon/ghinstallation"
+	"github.com/google/go-github/v38/github"
+	"golang.org/x/oauth2"
+)
+
+const (
+	appID          = 334984       // Replace with your app ID
+	installation   = 37605233     // Replace with your installation ID
+	repoOwner      = "artaasadi"  // Replace with the owner of the repository
+	repoName       = "trader-bot" // Replace with the name of the repository
+	privateKeyFile = "arta-test-github-app.2023-05-17.private-key.pem"
+)
+
+var (
+	privateKey *rsa.PrivateKey
+)
+
+func main() {
+	// Load the private key
+	keyBytes, err := ioutil.ReadFile(privateKeyFile)
+	if err != nil {
+		log.Fatalf("Failed to read private key: %v", err)
+	}
+	// privateKey, err = jwt.ParseRSAPrivateKeyFromPEM(keyBytes)
+	// if err != nil {
+	// 	log.Fatalf("Failed to parse private key: %v", err)
+	// }
+
+	// Create a GitHub client using the app's private key
+	tr, err := ghinstallation.NewAppsTransport(http.DefaultTransport, appID, keyBytes)
+	if err != nil {
+		log.Fatalf("Failed to parse private key: %v", err)
+	}
+	client := github.NewClient(&http.Client{Transport: tr})
+
+	// Get the installation token for the repository
+	token, _, err := client.Apps.CreateInstallationToken(context.Background(), installation, nil)
+	if err != nil {
+		log.Fatalf("Failed to create installation token: %v", err)
+	}
+
+	// Create a GitHub client using the installation token
+	tc := oauth2.NewClient(context.Background(), oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token.GetToken()},
+	))
+	client = github.NewClient(tc)
+
+	// Get the contents of a file in the repository
+	fileContent, _, _, err := client.Repositories.GetContents(context.Background(), repoOwner, repoName, "main.py", nil)
+	if err != nil {
+		log.Fatalf("Failed to get file contents: %v", err)
+	}
+
+	// Print the contents of the file
+	content, err := fileContent.GetContent()
+	if err != nil {
+		log.Fatalf("Failed to parse private key: %v", err)
+	}
+	fmt.Printf("File contents: %s\n", content)
+}
